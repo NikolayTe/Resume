c4a390929aa4b0d8f958561cdcba2eda316e2222
NikolayTe <nikolay.tesovec@gmail.com>
2025-11-19 06:16:11 +0300

    pppp

--- mail_server/config.py
+++
@@ -1,6 +0,0 @@
-SMPT_SERVER = "smtp.gmail.com"  # Адрес почтового сервера Gmail для отправки писем
-SMPT_PORT = 587  # Порт для защищенного соединения с почтовым сервером
-SENDER_EMAIL = "nikolay.tesovec@gmail.com"  # Email отправителя (ваша почта)
-SENDER_PASSWORD = "rmpy lljk atzv tvyk"  # Пароль приложения (не основной пароль!)
-PDF_PATH = 'mail_server/resume.pdf'
-LOGS_PATH = 'mail_server/mail_logs.txt'
